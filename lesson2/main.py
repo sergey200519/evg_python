2cdb790ea74e1b4202ab7819938049583be9ac34
sergey <oborok05@bk.ru>
2023-12-07 18:14:49 +0300

    lesson2

---
+++ lesson2/main.py
@@ -0,0 +1,12 @@
+string = "abcdefghijklmnopqrstuvwxyz"
+num =  5555
+
+# цыклы
+# for item in string:
+#     print(item)
+
+print(len(string), type(string))
+i = 0
+while i < len(string):
+    print(string[i])
+    i += 1
