88041bd3e8c65681e9c83b830abb2609fe5e671c
sergey <oborok05@bk.ru>
2023-12-17 20:02:51 +0300

    lesson4

---
+++ lesson4/main.py
@@ -0,0 +1,34 @@
+# Разработать функцию replace(). Желательно в этой функции использовать ранее разработаную функцию slice()
+# функция принимает 3 параметра:
+# исходная строка
+# что мы хотим изменить
+# на что мы хотим изменить
+# Пример: replace("abc", "ab", "d") ---> функция возвращает: "dc"
+# Встроеный в python метод .replace() использовать нельзя.
+
+# print("abcab".replace("ab", "!"))
+def slice(row, start, end):
+    answer = ""
+    i = start
+    while i < end:
+        answer += row[i]
+        i += 1
+    return answer
+# print(slice("abc", 0, 2)) # 0 1
+# print(slice("abc", 1, 2))
+
+def replace(row, tochange, replacement):
+    answer = ""
+    i = 0
+    while i < len(row):
+        if slice(row, i, i + len(tochange) - 1) == tochange:
+            answer = replacement
+        else:
+            answer += row[i]
+        i += 1
+    return answer
+
+
+print(replace("abc", "ab", "d") )
+
+# print(len("abc"))
